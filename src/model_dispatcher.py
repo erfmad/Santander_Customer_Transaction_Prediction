b64ec774eb96f1d31c26c74c46d98f00a82a932f
Erfan Majd <54864842+erfmad@users.noreply.github.com>
2021-06-29 17:45:35 -0400

    Update model_dispatcher.py

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,1 +1,14 @@
+from sklearn import tree
+from sklearn import ensemble
+import xgboost as xgb 
+from sklearn import linear_model
 
+
+models = {
+    "tree": tree.DecisionTreeClassifier(),
+    "rf": ensemble.RandomForestClassifier(),
+    "xgb1": xgb.XGBClassifier(),
+    "lin": linear_model.LogisticRegression()
+   
+
+}
