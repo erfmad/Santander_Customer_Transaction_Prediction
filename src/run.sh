bfcf98da8c5722f360b0f0c5bad06b114832b05f
Erfan Majd <54864842+erfmad@users.noreply.github.com>
2021-06-29 17:46:36 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,5 @@
-
+python train.py --fold 0 --model xgb1
+python train.py --fold 1 --model xgb1
+python train.py --fold 2 --model xgb1
+python train.py --fold 3 --model xgb1
+python train.py --fold 4 --model xgb1
